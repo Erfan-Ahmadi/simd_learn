336c2616f71e558554084b7078c9d4961c6133e3
Erfan Ahmadi <ahmadierfan99@gmail.com>
2019-06-05 16:44:16 +0430

    hadd

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <immintrin.h>
 
 int main()
 {
-	std::cout << "Hello, SIMD!";
+	std::cout << "Hello, SIMD!" << std::endl;
+
+	__m256 first = _mm256_setr_ps(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0);
+	__m256 second = _mm256_setr_ps(2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+	
+	__m256 result = _mm256_hadd_ps(first, second);
+	
+	float* result_arr = reinterpret_cast<float*>(&result);
+
+	for (size_t i = 0; i < 8; ++i)
+		std::cout << i << " = " << result_arr[i] << std::endl;
 }
